66c12f59785259b76ad145a93500543dc7fb3e79
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-03 15:03:15 -0400

    give the callback function a more concise name

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) { 
+const breedDetailsFromFile = function(breed, done) { 
   // a callback function such as this is usually given a more concise name such as 'done'
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
@@ -8,7 +8,7 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
     // if (!error) return data;
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     // CHANGE: Pass data into callback instead of returning it directly
-    if (!error) functionToRunWhenThingsAreDone(data);
+    if (!error) done(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
